0d667cc744330c03ec65927e45f540c3185dda09
dwSun <dwSun@Capsule>
2018-02-06 14:55:24 +0800

    hack tinymind working path

--- experiment.py
+++ experiment.py
@@ -21,6 +21,13 @@ from __future__ import print_function
 
 import os
 import sys
+
+print('current working dir [{0}]'.format(os.getcwd()))
+w_d = os.path.dirname(os.path.abspath(__file__))
+print('change wording dir to [{0}]'.format(w_d))
+os.chdir(w_d)
+sys.path.insert(0, w_d)
+
 import time
 
 import numpy as np
